62ceada9790209a9c9ea93c71c92a8b0556930fd
Amit Kumar Yadav <49604010+amit1github@users.noreply.github.com>
2022-04-22 18:26:02 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './Components/Header';
+import HomePage from "./Pages/HomePage"
+import CoinPage from './Pages/CoinPage';
+import PageNotFound from './Pages/PageNotFound';
 
-function App() {
+//check here if something happens with dark mode
+// const useStyles = styled(() => ({
+//   App: {
+//     backgroundColor: "#14161a",
+//     color: "white",
+//     minHeight: "100vh",
+//   },
+// }))
+
+const App = () => {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <BrowserRouter>
+    <div style={{ backgroundColor: "#14161a", color: "white", minHeight: "100vh" }}>
+      <Header/>
+      <Routes>
+        <Route path='/' element={<HomePage/>} />
+        <Route path='/coins/:id' element={<CoinPage/>} />
+        <Route path='*' element={<PageNotFound/>} />
+      </Routes>
     </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Components/Header.js
@@ -0,0 +1,63 @@
+import React from "react";
+import {
+  AppBar,
+  Container,
+  createTheme,
+  MenuItem,
+  Select,
+  ThemeProvider,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { CryptoState } from "../CryptoContext";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
+const Header = () => {
+  let navigate = useNavigate();
+
+  const { currency, setCurrency } = CryptoState();
+
+  console.log(currency);
+
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <AppBar color="transparent" position="static">
+        <Container>
+          <Toolbar>
+            <Typography
+              variant="h6"
+              onClick={() => navigate("/")}
+              sx={{
+                flexGrow: 1,
+                color: "gold",
+                fontFamily: "Montserrat",
+                fontWeight: "bold",
+                cursor: "pointer",
+              }}
+            >
+              Crypto Hunter
+            </Typography>
+
+            <Select
+              variant="outlined"
+              style={{ width: 100, height: 40, marginRight: 15 }}
+              value={currency}
+              onChange={(e) => setCurrency(e.target.value)}
+            >
+              <MenuItem value={"USD"}>USD</MenuItem>
+              <MenuItem value={"INR"}>INR</MenuItem>
+            </Select>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </ThemeProvider>
+  );
+};
+
+export default Header;
