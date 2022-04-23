afd94489591932d2c5a8082e42f1ec253a53e8e6
Amit Kumar Yadav <49604010+amit1github@users.noreply.github.com>
2022-04-24 03:16:28 +0530

    change title

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -41,7 +41,7 @@ const Header = () => {
                 cursor: "pointer",
               }}
             >
-              Crypto Hunter
+              Crypto Watcher
             </Typography>
 
             <Select
